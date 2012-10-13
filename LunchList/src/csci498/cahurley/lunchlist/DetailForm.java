f43ad70d4d4d608d4e91e602fe017eab50a9230e
Caitlin Hurley <caitlin.jhurley@gmail.com>
2012-10-13 13:37:42 -0600

    users can save the gps location of a restaurant; this only works if the restaurant has already been saved to the db

--- LunchList/src/csci498/cahurley/lunchlist/DetailForm.java
+++ LunchList/src/csci498/cahurley/lunchlist/DetailForm.java
@@ -3,16 +3,18 @@ package csci498.cahurley.lunchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailForm extends Activity 
@@ -21,8 +23,10 @@ public class DetailForm extends Activity
 	EditText address = null;
 	EditText notes = null;
 	EditText feed = null;
+	TextView location = null;
 	RadioGroup types = null;
 	RestaurantHelper helper = null;
+	LocationManager locationManager = null;
 	String restaurantId = null;
 	
 	@Override
@@ -38,6 +42,8 @@ public class DetailForm extends Activity
 		{
 			load();
 		}
+		
+		locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 	}
 	
 	@Override
@@ -57,8 +63,9 @@ public class DetailForm extends Activity
     	name = (EditText)findViewById(R.id.name_edit_text);
     	address = (EditText)findViewById(R.id.address_edit_text);
     	feed = (EditText)findViewById(R.id.feed);
-    	types = (RadioGroup)findViewById(R.id.types_of_restaurants_radio);
     	notes = (EditText)findViewById(R.id.notes_edit_text);
+    	location = (TextView)findViewById(R.id.location_text_view);
+    	types = (RadioGroup)findViewById(R.id.types_of_restaurants_radio);    	
     }
     
 	@Override
@@ -76,6 +83,7 @@ public class DetailForm extends Activity
 	public void onPause()
 	{
 		save();
+		locationManager.removeUpdates(onLocationChange);
 		
 		super.onPause();
 	}
@@ -97,6 +105,17 @@ public class DetailForm extends Activity
     }
     
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+    	if (restaurantId == null)
+    	{
+    		menu.findItem(R.id.location).setEnabled(false);
+    	}
+    	
+    	return super.onPrepareOptionsMenu(menu);
+    }
+    
+    @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
     	if (item.getItemId() == R.id.feed)
@@ -115,8 +134,14 @@ public class DetailForm extends Activity
     		
     		return true;
     	}
+    	else if (item.getItemId() == R.id.location)
+    	{
+    		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, onLocationChange);
+    		
+    		return true;
+    	}
     	
-    	return (super.onOptionsItemSelected(item));
+    	return super.onOptionsItemSelected(item);
     }
     
     private boolean isNetworkAvailable()
@@ -136,6 +161,7 @@ public class DetailForm extends Activity
     	address.setText(helper.getAddress(cursor));
     	notes.setText(helper.getNotes(cursor));
     	feed.setText(helper.getFeed(cursor));
+    	location.setText(String.format("%s, %s", String.valueOf(helper.getLatitude(cursor)), String.valueOf(helper.getLongitude(cursor))));
     	
     	String type = helper.getType(cursor);
     	if(type.equals("sit_down"))
@@ -186,4 +212,31 @@ public class DetailForm extends Activity
 				return("sit_down");
 		}
 	}
+	
+	LocationListener onLocationChange = new LocationListener()
+	{
+		public void onLocationChanged(Location fix)
+		{
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.format("%s, %s", String.valueOf(fix.getLatitude()), String.valueOf(fix.getLongitude())));
+			locationManager.removeUpdates(onLocationChange);
+			
+			Toast.makeText(DetailForm.this, "Location Saved", Toast.LENGTH_LONG).show();
+		}
+		
+		public void onProviderDisabled(String provider)
+		{
+			
+		}
+		
+		public void onProviderEnabled(String provider)
+		{
+			
+		}
+		
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+			
+		}
+	};
 }
