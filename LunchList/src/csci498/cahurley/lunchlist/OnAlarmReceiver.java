312aea7ed65209a5088b39c7f93561852065a753
Caitlin Hurley <caitlin.jhurley@gmail.com>
2012-10-29 16:27:50 -0600

    added new preference so user can choose if they want their lunch alarm to show up as a full-screen activity or notification. the default is the notification

--- LunchList/src/csci498/cahurley/lunchlist/OnAlarmReceiver.java
+++ LunchList/src/csci498/cahurley/lunchlist/OnAlarmReceiver.java
@@ -1,17 +1,42 @@
 package csci498.cahurley.lunchlist;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver
 {
+	private static final int NOTIFY_ME_ID = 1337;
+	
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		Intent i = new Intent(context, AlarmActivity.class);
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		boolean useNotification = preferences.getBoolean("use_notification", true);
 		
-		context.startActivity(i);
+		if (useNotification)
+		{
+			NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification note = new Notification(R.drawable.stat_notify_chat, "It's time for lunch!", System.currentTimeMillis());
+			PendingIntent i = PendingIntent.getActivity(context, 0, new Intent(context, AlarmActivity.class), 0);
+			
+			note.setLatestEventInfo(context, "LunchList", "It's time for lunch! Aren't you hungry?", i);
+			note.flags |= Notification.FLAG_AUTO_CANCEL;
+			
+			manager.notify(NOTIFY_ME_ID, note);
+		}
+		else
+		{
+			Intent i = new Intent(context, AlarmActivity.class);
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			
+			context.startActivity(i);
+		}
 	}
+	
 }
