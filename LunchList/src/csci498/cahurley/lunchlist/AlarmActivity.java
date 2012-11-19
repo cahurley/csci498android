aa4f8ce168a96f3eaa09e7f0485f61765fdf32ac
Caitlin Hurley <caitlin.jhurley@gmail.com>
2012-11-19 14:22:00 -0700

    added the ringtone to AlarmActivity so the ringtone plays until it ends or until the AlarmActivity is closed

--- LunchList/src/csci498/cahurley/lunchlist/AlarmActivity.java
+++ LunchList/src/csci498/cahurley/lunchlist/AlarmActivity.java
@@ -1,14 +1,57 @@
 package csci498.cahurley.lunchlist;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class AlarmActivity extends Activity
+public class AlarmActivity extends Activity implements OnPreparedListener
 {
+	MediaPlayer player = new MediaPlayer();
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alarm);
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		String sound = preferences.getString("alarm_ringtone", null);
+		if (sound != null)
+		{
+			player.setAudioStreamType(AudioManager.STREAM_ALARM);
+			
+			try
+			{
+				player.setDataSource(sound);
+				player.setOnPreparedListener(this);
+				player.prepareAsync();
+			}
+			catch (Exception ex)
+			{
+				Log.e("LunchList", "Exception in playing ringtone", ex);
+			}
+		}
 	}
+	
+	@Override
+	public void onPause()
+	{
+		if (player.isPlaying())
+		{
+			player.stop();
+		}
+		
+		super.onPause();
+	}
+	
+	public void onPrepared(MediaPlayer player)
+	{
+		player.start();
+	}
+	
 }
